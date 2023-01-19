54a54ee636beeb5cc000afd6349c1ca05fb01f18
neit <amiradilov202@gmail.com>
2023-01-19 11:51:40 +0600

    v

---
+++ Variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4
+x = "Sally"
+print(x)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+# A will not overwrite a
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
