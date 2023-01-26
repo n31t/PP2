926ca3492e71d526a3d187854d8ef7b0e5c82be7
neit <amiradilov202@gmail.com>
2023-01-26 11:34:09 +0600

    v

---
+++ week2/Lists.py
@@ -0,0 +1,28 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+list1 = ["abc", 34, True, 40, "male"]
+
+print(list1)
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+# note the double round-brackets
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
--- week2/While loops.py
+++ week2/While loops.py
@@ -2,3 +2,39 @@ i = 1
 while i < 6:
     print(i)
     i += 1
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+"""e x e r c i s e s"""
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
