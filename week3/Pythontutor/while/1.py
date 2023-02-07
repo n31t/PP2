7092510c56372b894d60e1d4b5bfe24e1bfdd9d9
neit <amiradilov202@gmail.com>
2023-02-08 01:14:07 +0600

    excuse

--- week3/Pythontutor/while/1.py
+++ week3/Pythontutor/while/1.py
@@ -1,6 +1,6 @@
 '''
 It was quite late to do this tasks
-Ikr they are really sample,but I just wasn't able
+Ikr they are really too simple ,but I just wasn't able
 to finish it on time (deadline)
 
 That's my excuse
